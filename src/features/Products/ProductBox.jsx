e2b6ab5d26ecb9b40095ad388306a6a8e3419f57
AmirHasanFiroozi <firooziamir224>
2025-02-03 11:43:07 +0330

    update-4.2

--- src/features/Products/ProductBox.jsx
+++ src/features/Products/ProductBox.jsx
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { requestProducts } from "../ProductRequest";
 import Product from "./Product";
 import ProductLoading from "./ProductLoading";
-import ProductSorting from "./ProductSorting";
 import useSize from "../../Hooks/useSize";
 
 function ProductBox() {
@@ -60,7 +59,7 @@ function ProductBox() {
     <div className="flex w-[80%] flex-col max-lg:w-full">
       <div
         ref={gridElement}
-        className="mb-10 grid w-[80%] grid-cols-4 border-t-[1px] border-(--color-gray2) max-xl:grid-cols-3 max-lg:w-full max-lg:grid-cols-2 max-sm:grid-cols-1"
+        className="mb-10 grid w-full grid-cols-4 border-t-[1px] border-(--color-gray2) max-xl:grid-cols-3 max-lg:w-full max-lg:grid-cols-2 max-sm:grid-cols-1"
       >
         {isLoading === true && (
           <>
